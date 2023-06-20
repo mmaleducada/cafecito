058996b524a4d65d3720860376d24f5442db382c
Malena de Arriba <dearribamalena@gmail.com>
2023-06-20 19:56:53 -0300

    agregue la lista de rutas al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,26 @@ import DetalleProducto from "./components/views/DetalleProducto";
 import CrearProducto from "./components/views/producto/CrearProducto";
 import EditarProducto from "./components/views/producto/EditarProducto";
 import Administrador from "./components/views/Administrador";
+import Registro from "./components/views/Registro"
+import Login from "./components/views/Login"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu></Menu>
-      {/* <Error404></Error404> */}
-      {<Inicio></Inicio>}
-      {/* <DetalleProducto></DetalleProducto> */}
-      {/* <CrearProducto></CrearProducto> */}
-      {/* <EditarProducto></EditarProducto> */}
-      {/* <Administrador></Administrador> */}
+      <Routes>
+        <Route exact path="/" element= {<Inicio></Inicio>} ></Route>
+        <Route exact path="/registro" element= {<Registro></Registro>} ></Route>
+        <Route exact path="/detalle" element= {<DetalleProducto></DetalleProducto>} ></Route>
+        <Route exact path="/login" element= {<Login></Login>} ></Route>
+        <Route exact path="/administrador" element= {<Administrador></Administrador>} ></Route>
+        <Route exact path="/administrador/crearProducto" element= {<CrearProducto></CrearProducto>} ></Route>
+        <Route exact path="/administrador/editarProducto" element= {<EditarProducto></EditarProducto>} ></Route>
+        <Route  path="*" element= {<Error404></Error404>} ></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
